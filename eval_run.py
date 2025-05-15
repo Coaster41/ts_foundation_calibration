cac32544055b3415d5606d6dcd67baf80bf81a60
Coen Adler <co41co@gmail.com>
2025-05-14 17:18:23 -0700

    eval plots

--- eval_run.py
+++ eval_run.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import time
 from utils.evals import *
+from collections import defaultdict
 
 UNIT_DICT = {"amazon-google": "H", "m5": "D", "glucose": "T", "meditation": "S"}
 UNIT_NUM_DICT = {"amazon-google": 1, "m5": 1, "glucose": 5, "meditation": 1}
@@ -41,17 +42,53 @@ if __name__ == "__main__":
     results = []
     confidence = 0.8
     start_time = time.time()
+    # for dataset in datasets:
+    #     for model in models:
+    #         print(f"Time: {time.time()-start_time:.4f}\t{dataset} {model}")
+    #         data_df, results_df, upper_df, lower_df, freq_delta, quantiles_dict = load_results(dataset, model, confidence)
+    #         mase_avg, mase_arr = mase(results_df, data_df, freq_delta)
+    #         results.append([dataset, model, 'mase', mase_avg, *mase_arr])
+    #         tce_avg, tce_arr = tce(lower_df, upper_df, data_df, freq_delta, confidence)
+    #         results.append([dataset, model, 'tce', tce_avg, *tce_arr])
+    #         wql_avg, wql_arr = wql(quantiles_dict, data_df, freq_delta)
+    #         results.append([dataset, model, 'wql', wql_avg, *wql_arr])
+    #         msis_avg, msis_arr = msis(lower_df, upper_df, data_df, freq_delta, confidence)
+    #         results.append([dataset, model, 'msis', msis_avg, *tce_arr])
+    # df = pd.DataFrame(results, columns=['dataset', 'model', 'metric', 'avg_result', *[str(h) for h in range(1,49)]])
+    # df.to_csv('model_results/metric_results.csv')
+
+    # groupby unique_id
+    max_ids = 0
     for dataset in datasets:
         for model in models:
             print(f"Time: {time.time()-start_time:.4f}\t{dataset} {model}")
             data_df, results_df, upper_df, lower_df, freq_delta, quantiles_dict = load_results(dataset, model, confidence)
-            mase_avg, mase_arr = mase(results_df, data_df, freq_delta)
-            results.append([dataset, model, 'mase', mase_avg, *mase_arr])
-            tce_avg, tce_arr = tce(lower_df, upper_df, data_df, freq_delta, confidence)
-            results.append([dataset, model, 'tce', tce_avg, *tce_arr])
-            wql_avg, wql_arr = wql(quantiles_dict, data_df, freq_delta)
-            results.append([dataset, model, 'wql', wql_avg, *wql_arr])
-            msis_avg, msis_arr = msis(lower_df, upper_df, data_df, freq_delta, confidence)
-            results.append([dataset, model, 'msis', msis_avg, *tce_arr])
-    df = pd.DataFrame(results, columns=['dataset', 'model', 'metric', 'avg_result', *[str(h) for h in range(1,49)]])
-    df.to_csv('model_results/metric_results.csv')
+            grouped_data_df = data_df.groupby('unique_id')
+            grouped_results_df = results_df.groupby('unique_id')
+            grouped_upper_df = upper_df.groupby('unique_id')
+            grouped_lower_df = lower_df.groupby('unique_id')
+            grouped_quantiles_dict = defaultdict(dict)
+            for quantile, quantile_df in quantiles_dict.items():
+                grouped_quantile_df = quantile_df.groupby('unique_id')
+                for unique_id, grouped_q in grouped_quantile_df:
+                    grouped_quantiles_dict[unique_id][quantile] = grouped_q
+            max_ids = max(max_ids, len(grouped_data_df))
+
+
+            mase_arr, tce_arr, msis_arr, wql_arr = list(), list(), list(), list()
+            for (unique_id, data_df), (_, results_df), (_, lower_df), (_, upper_df), (_, quantiles_dict) in \
+                zip(grouped_data_df, grouped_results_df, grouped_lower_df, grouped_upper_df, grouped_quantiles_dict.items()):
+                mase_avg, _ = mase(results_df, data_df, freq_delta)
+                mase_arr.append(mase_avg)
+                tce_avg, _ = tce(lower_df, upper_df, data_df, freq_delta, confidence)
+                tce_arr.append(tce_avg)
+                msis_avg, _ = msis(lower_df, upper_df, data_df, freq_delta, confidence)
+                msis_arr.append(msis_avg)
+                wql_avg, _ = wql(quantiles_dict, data_df, freq_delta)
+                wql_arr.append(wql_avg)
+            results.append([dataset, model, 'mase', *mase_arr])
+            results.append([dataset, model, 'tce', *tce_arr])
+            results.append([dataset, model, 'msis', *tce_arr])
+            results.append([dataset, model, 'wql', *wql_arr])
+    df = pd.DataFrame(results, columns=['dataset', 'model', 'metric', *[str(h) for h in range(1,max_ids+1)]])
+    df.to_csv('model_results/metric_results_unique_id.csv')
